0fff8908698a08d8d04cf925f73f29908cf9c0f4
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-11 20:29:32 +0400

    Create the post file and roots for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:id/posts/:id', to: 'posts#show'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
