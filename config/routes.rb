fdf3e0df0c9c394f2b464c265db2914c8d45109f
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 01:24:34 +0400

    Write correct roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/users/:id/posts', to: 'posts#index'
-  get '/users/:id/posts/:id', to: 'posts#show'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
