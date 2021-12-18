0db644caced2e43b972621fa7a85ccc06b282641
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 21:08:38 +0400

    Modify comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,18 @@
 class CommentsController < ApplicationController
-  def comments; end
+  skip_before_action :verify_authenticity_token
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+  end
+
+  private
+
+  def comment_params
+    params.permit(:text)
+  end
 end
