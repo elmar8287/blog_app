9e816d47f7abb56668e28a5499aad991f211338d
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 21:59:13 +0400

    Add spec for likes model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation for likes' do
+    subject { Like.new(author_id: 1, post_id: 1) }
+
+    before { subject.save }
+
+    it 'checks if post id is a number' do
+      subject.post_id = 'look'
+      expect(subject).to_not be_valid
+    end
+  end
 end
