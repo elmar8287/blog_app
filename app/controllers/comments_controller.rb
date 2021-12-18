6bbd40d10682d449826064a561b2c6d63e995308
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 21:09:03 +0400

    Add condition for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,12 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.author_id = current_user.id
     @comment.post_id = params[:post_id]
+    if @comment.save
+      flash[:notice] = 'Saved!'
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
   end
 
   private
