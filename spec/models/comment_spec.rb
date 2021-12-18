17e7ac400c198caf08ca1f3292d13c215a5265c2
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 21:57:31 +0400

    Add spec for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation for comments' do
+    subject { Comment.new(text: 'Good job', author_id: 1, post_id: 1) }
+
+    before { subject.save }
+
+    it 'checks if title is valid' do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'checks if post id is a number' do
+      subject.post_id = 'look'
+      expect(subject).to_not be_valid
+    end
+  end
 end
