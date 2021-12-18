da24327427e21aa3544b5c3994b0b610381714f3
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 22:02:52 +0400

    Add spec for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation for posts' do
+    user = User.create(name: 'Elik', bio: 'Test user')
+    subject do
+      Post.new(title: 'Elik`s post', text: 'This is text post', author_id: user)
+    end
+
+    before { subject.save }
+
+    it 'Is counter a number' do
+      subject.comments_counter = 'jdj'
+      expect(subject).to_not be_valid
+    end
+  end
 end
