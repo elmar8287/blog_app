0180266f26b8afec2d6b82e2b669aceac7ab1ef2
Elmar Abdulkarimov <elmar@Elmars-MacBook-Air.local>
2021-12-18 11:30:18 +0400

    Check the seeds recorded

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@ elik = User.create(name: "Test user", photo: "image.jpg", bio: "Thi is  bio abou
 
 elik_post = Post.create(title: "Elik`s post", text: "This is first post and here is nothing importante", author: elik)
 
+elik_comment = Comment.create(text: "Very nice post. Good job", author: elik, post: elik_post)
+
+p "Recorded"
