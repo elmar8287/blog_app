ecbc5890fa3b3f25248cfdbc74f56a6802a95c30
Elmar <L-mar@inbox.ru>
2021-12-26 02:39:33 +0400

    Fix linters

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,9 +1,8 @@
 class Api::PostsController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      @posts = Post.all
-      render json: @posts
-    end
+  before_action :authenticate_user!
+
+  def index
+    @posts = Post.all
+    render json: @posts
   end
-  
+end
